2b7375474290585effca7b41cecf998d11374778
IslamAshraf <islameliwa@gmail.com>
2022-03-21 10:13:35 +0200

    addurl and authorization middleware

--- modules/urls/contoller/addUrl.js
+++ modules/urls/contoller/addUrl.js
@@ -3,8 +3,16 @@ import { Url } from "../model/urlsModel.js";
 const addUrl = async (req, res) => {
   try {
     const { name, url } = req.body;
-    console.log(name, url);
-  } catch (error) {}
+    // console.log(req.user);
+    const newUrl = await Url.insertMany({
+      name,
+      url,
+      owner: req.user._id,
+    });
+    res.status(201).json({ message: "urlcheck created", data: newUrl });
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 };
 
 export { addUrl };
