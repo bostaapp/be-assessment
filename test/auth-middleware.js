1a6e122108037557f27a016e2ec68fcc6fe14fce
Yahia Mahmoud Abdelrahman Bassyouny <66659379+PricelessCodes@users.noreply.github.com>
2023-02-22 17:16:04 +0200

    added mocha-chai-sinon

---
+++ test/auth-middleware.js
@@ -0,0 +1,7 @@
+const authMiddleware = require('../middleware/auth');
+//check for tests (success or fail)
+const expect = require('chai').expect;
+
+it("Should throw an error if Authentication token is not exist", function() {
+    
+})
