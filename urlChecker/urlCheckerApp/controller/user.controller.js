fa36be434e6db9d481f61a7eb503108ccb75c0be
Omar Aref <omar.aref@sawa.ai>
2023-08-08 15:39:13 +0200

    added verification step to email, modified docker file to npm install every time we up the container

--- urlChecker/urlCheckerApp/controller/user.controller.js
+++ urlChecker/urlCheckerApp/controller/user.controller.js
@@ -4,7 +4,14 @@ const catchAsync = require('../utils/catchAsync');
 exports.register =catchAsync(async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const result = await userService.register(email, password);
-    return res.json(result)
+    const resultUser = await userService.register(email, password);
+    userService.sendEmail(resultUser);
+    return res.json(resultUser)
 
+})
+
+exports.verify = catchAsync(async (req, res, next) => {
+    const id = req.params.id;
+    const result = await userService.verify(id);
+    return res.json({"message" : "Email verified successfully, you can now login"})
 })
--- urlChecker/urlCheckerApp/service/user.service.js
+++ urlChecker/urlCheckerApp/service/user.service.js
@@ -1,4 +1,6 @@
 const User = require('../models/user.model')
+const nodeMailer = require('nodemailer')
+
 
 exports.register = async (email, password) => {
     const user = new User({
@@ -8,4 +10,45 @@ exports.register = async (email, password) => {
 
     const result = await user.save()
     return result
+}
+
+exports.sendEmail = (user) => {
+    const {
+        NODE_EMAIL,
+        NODE_EMAIL_PASSWORD,
+        
+    } = process.env;
+
+    const transporter = nodeMailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: "login",
+            user: NODE_EMAIL,
+            pass: NODE_EMAIL_PASSWORD
+        }
+    })
+
+    transporter.sendMail({
+        from: NODE_EMAIL,
+        to: user.email,
+        subject: 'Welcome to URL Checker, please verify your email',
+        text: 'Please verify your email by clicking the link below',
+        html: `<a href="http://localhost:8080/api/user/verify/${user._id}">Verify Email</a>`
+    }).then((info) => {
+        console.log(info)
+    }).catch((err) => {
+        console.log(err)
+    })
+
+}
+
+exports.verify = async (id) => {
+
+    //  find user by id and update the verified field to true 
+    const foundUser = await User.findByIdAndUpdate(id, {
+        emailVerified: true
+    }, {   
+        new: true
+    })
+    return foundUser
 }
